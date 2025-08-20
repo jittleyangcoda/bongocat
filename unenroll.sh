a54449ecb54883eb4e25f0116025353ad83cae8d
leoisbadatmakeingthings <107022214+leoisbadatmakeingthings@users.noreply.github.com>
2025-08-20 15:05:57 -0700

    Update unenroll.sh

--- unenroll.sh
+++ unenroll.sh
@@ -1,5 +1,7 @@
+echo "starting unenroll"
 echo --enterprise-enable-unified-state-determination=never >/tmp/chrome_dev.conf
 echo --enterprise-enable-forced-re-enrollment=never >>/tmp/chrome_dev.conf
 echo --enterprise-enable-initial-enrollment=never >>/tmp/chrome_dev.conf
 mount --bind /tmp/chrome_dev.conf /etc/chrome_dev.conf
 initctl restart ui
+echo "Finished"
